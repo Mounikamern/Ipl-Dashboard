0733d59cc7bf76f71641986fec3b9558c3a67b97
Mounikamern <mounipolamarasetti@gmail.com>
2024-03-19 13:17:21 +0000

    solution

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const MatchCard = props => {
+  const {matchDetails} = props
+  console.log(matchDetails)
+  const {result, competingTeamLogo, matchStatus, competingTeam} = matchDetails
+
+  return (
+    <li className="match-card">
+      <img
+        className="team-logo-img"
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+      />
+      <p className="team-name">{competingTeam}</p>
+      <p className="result">{result}</p>
+      <p className={`${matchStatus}`}>{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
